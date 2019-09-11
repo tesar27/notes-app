e7b06d1ccc183a541963546dd6530cec097787e5
yerbolattazhkeyev <ytazhkeyev@gmail.com>
2019-09-12 01:51:06 +0600

    initial commit

---
+++ resources/assets/js/components/TaskList.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class='row'>
+        <h1>My Notes</h1>
+        <h4>New Note</h4>
+        <form action="#" @submit.prevent="createTask()">
+            <div class="input-group">
+                <input v-model="task.title" type="text" name="title" class="form-control" autofocus>
+                <span class="input-group-btn">
+                    <button type="submit" class="btn btn-primary">New Task1</button>
+                </span>
+            </div>
+            <div class="form-group">
+            <label for="exampleFormControlTextarea1">Description</label>
+            <textarea class="form-control" v-model="task.body" name="body" id="exampleFormControlTextarea1" rows="3"></textarea>
+     		 </div>
+        </form>
+        <h4>All Notes</h4>
+        <div class="row text-center" v-if="loading">
+            <rotate-square2></rotate-square2>
+        </div>
+
+        <ul class="list-group" v-if="!loading">
+            <li v-if='list.length === 0'>There are no tasks yet!</li>
+            <li class="list-group-item" v-for="task in list" v-bind:key="task.index">
+
+                {{ task.body }}
+
+                <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">Delete</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { RotateSquare2 } from "vue-loading-spinner";
+
+export default {
+	components: {
+		RotateSquare2
+	},
+	data() {
+		return {
+			loading: true,
+			list: [],
+			task: {
+				id: "",
+				title: "",
+				body: ""
+			}
+		};
+	},
+
+	created() {
+		this.fetchTaskList();
+	},
+
+	methods: {
+		fetchTaskList() {
+			axios
+				.get("api/tasks")
+				.then(res => {
+					this.list = res.data;
+					this.loading = false;
+				})
+				.catch(err => console.error(err));
+		},
+
+		createTask() {
+			axios
+				.post("api/tasks", this.task)
+				.then(res => {
+					this.task.title = "";
+					this.task.body = "";
+				})
+				.catch(err => console.error(err));
+		},
+
+		deleteTask(id) {
+			axios
+				.delete("api/tasks/" + id)
+				.then(res => {
+					this.fetchTaskList();
+				})
+				.catch(err => console.error(err));
+		}
+	}
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" id='app'>
+    <!--    <task-list></task-list> -->
+
+    <div class="alert alert-primary" role="alert">
+  <h4 class="alert-heading"><b> Welcome to the NotesApp! </b></h4>
+  <p>At first, you should authorize.</p>
+  <hr>
+  <p class="mb-0">Always remember the Creater...</p>
+</div>
+    </div>
+@endsection
